e583741ad8284c112a1672d16aaf7a1e22c051aa
Aliu Raimi Tunde <raimialiu428@gmail.com>
2021-05-03 08:35:34 +0100

    added StackLogType

--- Configuration/StackLogType.cs
+++ Configuration/StackLogType.cs
@@ -2,6 +2,21 @@ namespace StackLog.Configuration
 {
     public class StackLogType
     {
-        
+        public const string StackInformation = "__STACKINFO__";
+        public const string StackDebug = "__STACKDEBUG__";
+        public const string StackWarn = "__STACKWARN__";
+        public const string StackFatal = "__STACKFATAL__";
+        public const string StackError = "__STACKERROR__";
+
+       
+    }
+
+    public class StackLogTypeCode
+    {
+        public const int StackInfoCode = 1;
+        public const int StackWarnCode = 2;
+        public const int StackDebugCode = 3;
+        public const int StackErrorCode = 4;
+        public const int StackFatalCode = 5;
     }
 }
