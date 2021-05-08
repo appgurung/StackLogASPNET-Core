4fc289e929e1a209524b8e0948b3799268a45769
NTNIGE\A241400 <olatunde.raimi@stanbicibtc.com>
2021-05-08 15:52:54 +0100

    sdk_rewrite

--- Configuration/StackLogConfiguration.cs
+++ Configuration/StackLogConfiguration.cs
@@ -18,6 +18,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using StackLog.HttpModule.Configuration;
+//using StackLog.HttpModule.Configuration;
 //using Microsoft.Web.Administration;
 
 namespace StackLog.Configuration
@@ -66,190 +68,190 @@ namespace StackLog.Configuration
         // mvc and web api filter here
         // .net core filter here
 
-        public class StackLogRequestInsight : System.Web.Mvc.ActionFilterAttribute
-        {
-            private IStackLog instance;
-            private StackLogResponse information; 
-            public StackLogRequestInsight()
-            {
-                //instance =  HttpContext.Items["StackLoggerInstance"] as StackLoggerMvc;
-                information = new StackLogResponse();
-                information.startTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
-            }
+        //public class StackLogRequestInsight : System.Web.Mvc.ActionFilterAttribute
+        //{
+        //    private IStackLog instance;
+        //    private StackLogResponse information; 
+        //    public StackLogRequestInsight()
+        //    {
+        //        //instance =  HttpContext.Items["StackLoggerInstance"] as StackLoggerMvc;
+        //        information = new StackLogResponse();
+        //        information.startTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
+        //    }
 
-        public override void OnResultExecuted(ResultExecutedContext filterContext)
-        {
+        //public override void OnResultExecuted(ResultExecutedContext filterContext)
+        //{
            
-            //if(filterContext.HttpContext.Response.Mo)
-            //information.httpResponse = filterContext.HttpContext.Response.
-            base.OnResultExecuted(filterContext);
-        }
+        //    //if(filterContext.HttpContext.Response.Mo)
+        //    //information.httpResponse = filterContext.HttpContext.Response.
+        //    base.OnResultExecuted(filterContext);
+        //}
 
-        public override void OnResultExecuting(ResultExecutingContext filterContext)
-        {
-            var endDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
-            information.endTime = endDate;
-            //filterContext.HttpContext;
-            information.statusCode = filterContext.HttpContext.Response.StatusCode;
-            var requestIdPool = new Random();
-            string id = "";
-            for (var i = 0; i < 6; i++)
-            {
-                var next = Math.Floor(Convert.ToDecimal(requestIdPool.Next(1, 100)));
-                id += next;
+        //public override void OnResultExecuting(ResultExecutingContext filterContext)
+        //{
+        //    var endDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
+        //    information.endTime = endDate;
+        //    //filterContext.HttpContext;
+        //    information.statusCode = filterContext.HttpContext.Response.StatusCode;
+        //    var requestIdPool = new Random();
+        //    string id = "";
+        //    for (var i = 0; i < 6; i++)
+        //    {
+        //        var next = Math.Floor(Convert.ToDecimal(requestIdPool.Next(1, 100)));
+        //        id += next;
 
-            }
+        //    }
 
-            information.requestId = id;
+        //    information.requestId = id;
 
-            var viewResult = filterContext.Result as ViewResult;
+        //    var viewResult = filterContext.Result as ViewResult;
 
-            if (viewResult != null)
-            {
-                if (viewResult.Model != null)
-                {
-                    information.httpResponse = JsonConvert.SerializeObject(viewResult.Model);
-                }
-                else
-                {
-                    var newViewResult = filterContext.Result as JsonResult;
+        //    if (viewResult != null)
+        //    {
+        //        if (viewResult.Model != null)
+        //        {
+        //            information.httpResponse = JsonConvert.SerializeObject(viewResult.Model);
+        //        }
+        //        else
+        //        {
+        //            var newViewResult = filterContext.Result as JsonResult;
 
-                    if (newViewResult != null)
-                    {
-                        if (newViewResult.Data != null)
-                        {
-                            information.httpResponse = JsonConvert.SerializeObject(newViewResult.Data);
-                        }
-                    }
-                }
+        //            if (newViewResult != null)
+        //            {
+        //                if (newViewResult.Data != null)
+        //                {
+        //                    information.httpResponse = JsonConvert.SerializeObject(newViewResult.Data);
+        //                }
+        //            }
+        //        }
 
-            }
-            else
-            {
-                var newViewResult = filterContext.Result as JsonResult;
+        //    }
+        //    else
+        //    {
+        //        var newViewResult = filterContext.Result as JsonResult;
 
-                if (newViewResult != null)
-                {
-                    if (newViewResult.Data != null)
-                    {
-                        information.httpResponse = JsonConvert.SerializeObject(newViewResult.Data);
-                    }
-                }
-            }
+        //        if (newViewResult != null)
+        //        {
+        //            if (newViewResult.Data != null)
+        //            {
+        //                information.httpResponse = JsonConvert.SerializeObject(newViewResult.Data);
+        //            }
+        //        }
+        //    }
 
-            if (instance != null)
-            {
-                instance.LogCloudWatch(information);
-            }
-            else
-            {
-                string secretKeyCode = ReadConfigSettings("StackLogSecretKey").ToString();
-                string bucketKeyCode = ReadConfigSettings("StackLogBucketKey").ToString();
-                string enableCloudWatch = ReadConfigSettings("enableCloudWatch").ToString();
-                string enableFileLog = ReadConfigSettings("enableFileLog").ToString();
-                string enableConsoleLogging = ReadConfigSettings("enableConsoleLogging").ToString();
-                //IConfigurationBuilder build = IConfigurationBuilder.
+        //    if (instance != null)
+        //    {
+        //        instance.LogCloudWatch(information);
+        //    }
+        //    else
+        //    {
+        //        string secretKeyCode = ReadConfigSettings("StackLogSecretKey").ToString();
+        //        string bucketKeyCode = ReadConfigSettings("StackLogBucketKey").ToString();
+        //        string enableCloudWatch = ReadConfigSettings("enableCloudWatch").ToString();
+        //        string enableFileLog = ReadConfigSettings("enableFileLog").ToString();
+        //        string enableConsoleLogging = ReadConfigSettings("enableConsoleLogging").ToString();
+        //        //IConfigurationBuilder build = IConfigurationBuilder.
 
-                bool isCloudWatchEnabled = false;
-                bool isFileLogEnabled = false;
-                bool isConsoleLogEnabled = false;
-                if (enableCloudWatch != null || enableCloudWatch != "")
-                {
-                    isCloudWatchEnabled = enableCloudWatch == "true" || enableCloudWatch == "TRUE" || enableCloudWatch == "True" ? true : false;
-                }
+        //        bool isCloudWatchEnabled = false;
+        //        bool isFileLogEnabled = false;
+        //        bool isConsoleLogEnabled = false;
+        //        if (enableCloudWatch != null || enableCloudWatch != "")
+        //        {
+        //            isCloudWatchEnabled = enableCloudWatch == "true" || enableCloudWatch == "TRUE" || enableCloudWatch == "True" ? true : false;
+        //        }
                 
-                if (enableFileLog != null || enableFileLog != "")
-                {
-                    isFileLogEnabled = enableFileLog == "true" || enableFileLog == "TRUE" || enableFileLog == "True" ? true : false;
-                }
+        //        if (enableFileLog != null || enableFileLog != "")
+        //        {
+        //            isFileLogEnabled = enableFileLog == "true" || enableFileLog == "TRUE" || enableFileLog == "True" ? true : false;
+        //        }
                 
-                if (enableConsoleLogging != null || enableConsoleLogging != "")
-                {
-                    isConsoleLogEnabled = enableFileLog == "true" || enableFileLog == "TRUE" || enableFileLog == "True" ? true : false;
-                }
+        //        if (enableConsoleLogging != null || enableConsoleLogging != "")
+        //        {
+        //            isConsoleLogEnabled = enableFileLog == "true" || enableFileLog == "TRUE" || enableFileLog == "True" ? true : false;
+        //        }
                 
                
-                instance = new StackLog(new StackLogOptions()
-                {
-                    secretKey = secretKeyCode,
-                    bucketKey = bucketKeyCode,
-                    enableCloudWatch = isCloudWatchEnabled,
-                    enableFileLogging = isFileLogEnabled,
-                    enableConsoleLogging = isConsoleLogEnabled
-                });
+        //        instance = new StackLog(new StackLogOptions()
+        //        {
+        //            secretKey = secretKeyCode,
+        //            bucketKey = bucketKeyCode,
+        //            enableCloudWatch = isCloudWatchEnabled,
+        //            enableFileLogging = isFileLogEnabled,
+        //            enableConsoleLogging = isConsoleLogEnabled
+        //        });
                 
-                instance.LogCloudWatch(information);
-            }
-            base.OnResultExecuting(filterContext);
-        }
+        //        instance.LogCloudWatch(information);
+        //    }
+        //    base.OnResultExecuting(filterContext);
+        //}
 
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            var responseStream = filterContext.Controller.ControllerContext.HttpContext.Response.OutputStream;
-            base.OnActionExecuted(filterContext);
-        }
+        //public override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    var responseStream = filterContext.Controller.ControllerContext.HttpContext.Response.OutputStream;
+        //    base.OnActionExecuted(filterContext);
+        //}
 
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
             
            
-            instance = filterContext.HttpContext.Items["StackLogInstance"] as StackLog;
-            if (instance == null)
-            {
-                throw new StackLogException(StackLogExceptionErrors.INSTANCE_REGISTRATION_FAILED);
-            }
+        //    instance = filterContext.HttpContext.Items["StackLogInstance"] as StackLog;
+        //    if (instance == null)
+        //    {
+        //        throw new StackLogException(StackLogExceptionErrors.INSTANCE_REGISTRATION_FAILED);
+        //    }
 
 
-            var uri = new Uri(filterContext.HttpContext.Request.Url.AbsoluteUri);
-            information.httpProtocol = uri.Scheme;
-            information.header = filterContext.HttpContext.Request.Headers.ToString();
-            information.userAgent = filterContext.HttpContext.Request.Headers["User-Agent"].ToString();
-            information.controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
-            information.isRequest = true;
-            information.controllerMethod = filterContext.ActionDescriptor.ActionName;
-            information.httpVerb = filterContext.HttpContext.Request.HttpMethod;
-            information.url = filterContext.HttpContext.Request.RawUrl;
-           // information.queryString = filterContext.HttpContext.Request.QueryString.ToString();
-            information.remoteIpAddress = filterContext.HttpContext.Request.UserHostAddress;
-            information.executedSqlQueries = "";
+        //    var uri = new Uri(filterContext.HttpContext.Request.Url.AbsoluteUri);
+        //    information.httpProtocol = uri.Scheme;
+        //    information.header = filterContext.HttpContext.Request.Headers.ToString();
+        //    information.userAgent = filterContext.HttpContext.Request.Headers["User-Agent"].ToString();
+        //    information.controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+        //    information.isRequest = true;
+        //    information.controllerMethod = filterContext.ActionDescriptor.ActionName;
+        //    information.httpVerb = filterContext.HttpContext.Request.HttpMethod;
+        //    information.url = filterContext.HttpContext.Request.RawUrl;
+        //   // information.queryString = filterContext.HttpContext.Request.QueryString.ToString();
+        //    information.remoteIpAddress = filterContext.HttpContext.Request.UserHostAddress;
+        //    information.executedSqlQueries = "";
 
-            var serverVaraibles = filterContext.HttpContext.Request.ServerVariables;
-            var attributes = filterContext.ActionDescriptor.GetCustomAttributes(true);
+        //    var serverVaraibles = filterContext.HttpContext.Request.ServerVariables;
+        //    var attributes = filterContext.ActionDescriptor.GetCustomAttributes(true);
 
-          //  var protocol = new Uri(filterContext.HttpContext.Request.RawUrl);
-            //information.httpProtocol = protocol.ToString();
-            StringBuilder methodAttributeInfo = new StringBuilder("{");
+        //  //  var protocol = new Uri(filterContext.HttpContext.Request.RawUrl);
+        //    //information.httpProtocol = protocol.ToString();
+        //    StringBuilder methodAttributeInfo = new StringBuilder("{");
 
-            var currentActionMethodFilterAttributes = filterContext.ActionDescriptor.GetFilterAttributes(true);
+        //    var currentActionMethodFilterAttributes = filterContext.ActionDescriptor.GetFilterAttributes(true);
 
-            foreach(var current in currentActionMethodFilterAttributes)
-            {
-                methodAttributeInfo.Append(current.GetType().Name + ", "); 
-            }
+        //    foreach(var current in currentActionMethodFilterAttributes)
+        //    {
+        //        methodAttributeInfo.Append(current.GetType().Name + ", "); 
+        //    }
 
-            foreach (var currentAttribute in attributes)
-            {
-                methodAttributeInfo.Append(currentAttribute.GetType().FullName + ", ");
-            }
+        //    foreach (var currentAttribute in attributes)
+        //    {
+        //        methodAttributeInfo.Append(currentAttribute.GetType().FullName + ", ");
+        //    }
 
-            methodAttributeInfo.Append("}");
-            information.methodAttributes = methodAttributeInfo.ToString();
-            StringBuilder paramInfo = new StringBuilder("{");
+        //    methodAttributeInfo.Append("}");
+        //    information.methodAttributes = methodAttributeInfo.ToString();
+        //    StringBuilder paramInfo = new StringBuilder("{");
 
-            foreach(var p in filterContext.ActionParameters)
-            {
+        //    foreach(var p in filterContext.ActionParameters)
+        //    {
                 
-                paramInfo.Append($"Name:{p.Key}, Value:{p.Value}");
-            }
-            paramInfo.Append("}");
-            information.queryString = paramInfo.ToString();
-            information.requestParam = filterContext.HttpContext?.Request.BodyToString();
-            //var param = filterContext.HttpContext.Request.R
+        //        paramInfo.Append($"Name:{p.Key}, Value:{p.Value}");
+        //    }
+        //    paramInfo.Append("}");
+        //    information.queryString = paramInfo.ToString();
+        //    information.requestParam = filterContext.HttpContext?.Request.BodyToString();
+        //    //var param = filterContext.HttpContext.Request.R
             
-            base.OnActionExecuting(filterContext);
-            //filterContext.
-            }
-        }
+        //    base.OnActionExecuting(filterContext);
+        //    //filterContext.
+        //    }
+        //}
         
         public class StackLogMiddleware
         {
@@ -299,43 +301,14 @@ namespace StackLog.Configuration
         }
        
 
-        public class StackLoggerGlobalExceptionHandler : ActionFilterAttribute, IExceptionFilter
-        {
-            public void OnException(ExceptionContext filterContext)
-            {
-                filterContext.ExceptionHandled = true;
-                Exception e = filterContext.Exception;
-                var instance = filterContext.HttpContext.Items["StackLogInstance"] as StackLog;
-
-                var methodInfo = e.GetType().FullName;
-            
-                if(instance == null)
-                {
-                    throw new StackLogException(StackLogExceptionErrors.INSTANCE_REGISTRATION_FAILED);
-                }
-
-                var secretKey = instance.Secret_Key;
-                var bucketKey = instance.Bucket_Key;
-
-                if(String.IsNullOrEmpty(secretKey) && String.IsNullOrEmpty(bucketKey))
-                {
-                    throw new StackLogException(StackLogExceptionErrors.KEY_MISSING);
-                }
-
-                instance.LogFatal(e);
-
-                filterContext.Result = new ViewResult()
-                {
-                    ViewName = instance.ErrorViewName
-                };
-            }
-        }
+       
         
         public static void RegisterStackLog(GlobalFilterCollection filter, dynamic context)
         {
-            context.Items.Add("StackLogInstance", new StackLog());
-            filter.Add(new StackLogConfiguration.StackLoggerGlobalExceptionHandler());
-            filter.Add(new StackLogRequestInsight());
+            var instance = new StackLog();
+            context.Items.Add("StackLogInstance", instance);
+            filter.Add(new StackLogGlobalExceptionHandler());
+            filter.Add(new StackLogWebMvcRequestInsight(instance));
         }
         
     }
